5058b3a656856cfad487ff62cedc3987f3927b44
Ben <benz303bb@gmail.com>
2021-12-22 18:30:32 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # zsh-npm
-Personal zsh plugin for npm
+
+Personal zsh plugin for npm.
+
+| Alias | Command
+| --- | ---
+| `n` | `noglob npm`
+| `ni` | `noglob npm install`
+| `nu` | `noglob npm update`
+| `nr` | `noglob npm run`
+| `ne` | `noglob npm exec`
+| `nd` | `noglob npm dev`
+| `nt` | `noglob npm test`
+| `ns` | `noglob npm start`
+| `nl` | `noglob npm lint`
+
+## Install
+
+Using [zpm](https://github.com/zpm-zsh/zpm)
+
+Add `zpm load zfben/zsh-npm` into .zshrc
---
+++ npm.plugin.zsh
@@ -0,0 +1,24 @@
+(( $+commands[npm] )) && {
+  rm -f "${ZSH_CACHE_DIR:-$ZSH/cache}/npm_completion"
+
+  _npm_completion() {
+    local si=$IFS
+    compadd -- $(COMP_CWORD=$((CURRENT-1)) \
+                 COMP_LINE=$BUFFER \
+                 COMP_POINT=0 \
+                 npm completion -- "${words[@]}" \
+                 2>/dev/null)
+    IFS=$si
+  }
+  compdef _npm_completion npm
+}
+
+alias n="noglob npm"
+alias ni="n install"
+alias nu="n update"
+alias nr="n run"
+alias ne="n exec"
+alias nd="n run dev"
+alias nt="n run test"
+alias ns="n run start"
+alias nl="n run lint"
